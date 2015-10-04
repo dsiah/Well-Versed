79f62c83cc21f9c1b0141f7985440d50cf28b888
David Siah <dsiah@villanova.edu>
2015-10-04 00:05:47 -0400

    more iterations on the UI

--- src/js/components/main.jsx
+++ src/js/components/main.jsx
@@ -3,7 +3,9 @@ const $ = require('jquery');
 const request = require('request');
 const React = require('react');
 const RaisedButton = require('material-ui/lib/raised-button');
-const Dialog = require('material-ui/lib/dialog');
+const List = require('material-ui/lib/lists/list');
+const ListItem = require('material-ui/lib/lists/list-item');
+const Divider = require('material-ui/lib/lists/list-divider');
 const ThemeManager = require('material-ui/lib/styles/theme-manager');
 const LightRawTheme = require('material-ui/lib/styles/raw-themes/light-raw-theme');
 const Colors = require('material-ui/lib/styles/colors');
@@ -34,9 +36,12 @@ const Main = React.createClass({
 
     this.setState({muiTheme: newMuiTheme});
     this.setState({
-      query: 'cached info goes here',
+      query: 'Cached Info Goes Here',
       imageUrl: 'http://www.gifbin.com/bin/500824yu29.gif',
-      blurb: 'bruce flea'
+      blurb: 'Bruce Flea was a Hong Kong American martial artist,' + 
+      ' Hong Kong action film actor, martial arts instructor, ' +
+      'philosopher, filmmaker, and the founder of Jeet Kune Do. ' + 
+      'Lee was the son of Cantonese opera star Lee Hoi-Chuen.'
     });
   },
 
@@ -54,9 +59,21 @@ const Main = React.createClass({
       <div style={containerStyle}>
         <h1>Well Versed</h1>
 
-        <Paper zDepth={1} className="query-subject">{this.state.query}</Paper>
         <img className="picture" src={this.state.imageUrl} />
-        <Paper zDepth={1} className="bio">{this.state.blurb}</Paper>
+
+        <Paper zDepth={1} className="bio">
+          <h3>{this.state.query}</h3>
+          <p>{this.state.blurb}</p>
+        </Paper>
+
+        <List zDepth={1} className="news-list" subheader="Related News"> 
+          <Divider />
+          <ListItem primaryText="News Item 1"></ListItem>
+          <Divider />
+          <ListItem primaryText="News Item 2"></ListItem>
+          <Divider />
+          <ListItem primaryText="News Item 3"></ListItem>
+        </List>
 
       </div>
     );
